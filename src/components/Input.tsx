a76f0ba244d111daae989d4d202a2ddb5ce9e9c7
matdelp <mathilde.delpeuch@gmail.com>
2025-06-14 09:31:58 +0200

    feat: integrate react-hook-form in Input and SignUpForm components for form handling

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,18 +1,21 @@
 import React from "react";
+import { useFormContext } from "react-hook-form";
 
 type InputProps = {
-    type:"text" | "password" | "email" | "number",
-    id: string
-    placeholder: string
-}
+  type: "text" | "password" | "email" | "number";
+  id: string;
+  placeholder: string;
+};
 
 export const Input: React.FC<InputProps> = ({ type, id, placeholder }) => {
+  const { register } = useFormContext();
   return (
     <input
       className="p-2 bg-background-500 rounded-md text-main-100 w-full"
       type={type}
       id={id}
       placeholder={placeholder}
+      {...register(id, { required: { value: true, message: "required" } })}
     />
   );
 };
--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,25 +1,38 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { FormProvider } from "react-hook-form";
 import { Input } from "./Input";
 
 export const SignUpForm: React.FC = () => {
+  const methods = useForm();
+
+  const onSubmit = methods.handleSubmit((data) => {
+    console.log(data);
+  });
+
   return (
     <>
-      <form
-        className="flex flex-col items-center justify-center gap-3 w-full"
-        action=""
-        method="post"
-      >
-        <Input type="text" id="firstName" placeholder="First Name" />
-        <Input type="text" id="lastName" placeholder="Last Name" />
-        <Input type="email" id="email" placeholder="Email" />
-        <Input type="password" id="password" placeholder="Password" />
-        <button
-          className="px-2 py-1 bg-blue-500 rounded-md text-main-100 w-full"
-          type="submit"
+      <FormProvider {...methods}>
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          noValidate
+          className="flex flex-col items-center justify-center gap-3 w-full"
+          action=""
+          method="post"
         >
-          Sign Up
-        </button>
-      </form>
+          <Input type="text" id="firstName" placeholder="First Name" />
+          <Input type="text" id="lastName" placeholder="Last Name" />
+          <Input type="email" id="email" placeholder="Email" />
+          <Input type="password" id="password" placeholder="Password" />
+          <button
+            className="px-2 py-1 bg-blue-500 rounded-md text-main-100 w-full"
+            type="submit"
+            onClick={onSubmit}
+          >
+            Sign Up
+          </button>
+        </form>
+      </FormProvider>
     </>
   );
 };
