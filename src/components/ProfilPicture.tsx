9d668a666c6079a5e604f9869c5a7868e43af97f
matdelp <mathilde.delpeuch@gmail.com>
2025-06-15 17:20:17 +0200

    Add ProfilPicture component for user avatar upload and display
    - Implemented a new component to allow users to upload and change their profile picture.
    - Utilized local state to manage the image URL and a ref to trigger file input.
    - Included a placeholder image and an edit icon for user interaction.

---
+++ src/components/ProfilPicture.tsx
@@ -0,0 +1,43 @@
+import React, { useRef, useState } from "react";
+import userPlaceholder from "../assets/images/userPlaceholder.jpg";
+import { FaPen } from "react-icons/fa";
+
+export const ProfilPicture = () => {
+  const [imgUrl, setImgUrl] = useState<string>("");
+  const refImg = useRef<HTMLInputElement>(null);
+
+  const handleImgChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const newImgUrl = URL.createObjectURL(file);
+      setImgUrl(newImgUrl);
+    }
+  };
+  const handleEditClick = () => {
+    if (refImg.current) {
+      refImg.current.click();
+    }
+  };
+
+  return (
+    <div className="flex justify-between">
+      <div className="flex w-20 h-20 relative">
+        <img
+          className="rounded-full object-cover w-20 h-20"
+          src={imgUrl || userPlaceholder}
+          alt="User avatar"
+        />
+        <button onClick={handleEditClick}>
+          <FaPen className="absolute bottom-0 right-0 w-3 h-3 text-gray-500 cursor-pointer" />
+        </button>
+        <input
+          type="file"
+          accept="image/*"
+          ref={refImg}
+          onChange={handleImgChange}
+          style={{ display: "none" }}
+        />
+      </div>
+    </div>
+  );
+};
--- src/pages/profil/ProfilPage.tsx
+++ src/pages/profil/ProfilPage.tsx
@@ -1,12 +1,33 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { LeaderHeader } from "../../components/LeaderHeader";
+import { ProfilPicture } from "../../components/ProfilPicture";
 
-export const ProfilPage = () => {
+const addUpperCase = (str: string): string =>
+  str.charAt(0).toUpperCase() + str.slice(1);
+
+export const ProfilPage: React.FC = () => {
   const { userId } = useParams();
+  const renderName = (userId: string | undefined): string => {
+    const [firstName = "", lastName = ""] = userId?.split("_") || [];
+    return `${addUpperCase(firstName)} ${addUpperCase(lastName)}`;
+  };
+
   return (
     <>
       <LeaderHeader />
+      <div className="p-5">
+        <div className="flex xl:flex-row justify-between items-center">
+          <ProfilPicture />
+          <p className="text-sm bg-gray-200 px-3 py-1 rounded-full self-center">
+            Rank: 24
+          </p>
+        </div>
+        <div>{renderName(userId)}</div>
+        <div>form</div>
+        <div>challenge</div>
+        <div>coding strike</div>
+      </div>
     </>
   );
 };
