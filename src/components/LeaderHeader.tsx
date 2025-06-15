897ea3e4a9fd0f06284ca1a1e8ce420fcfd77108
matdelp <mathilde.delpeuch@gmail.com>
2025-06-15 12:26:59 +0200

    fix: improve layout and styling of LeaderHeader and LeaderboardPage components for better responsiveness

--- src/components/LeaderHeader.tsx
+++ src/components/LeaderHeader.tsx
@@ -4,17 +4,19 @@ import { MdLightMode } from "react-icons/md";
 
 export const LeaderHeader: React.FC = () => {
   return (
-    <header className="flex justify-between py-5 px-4 bg-main-100">
-      <div className="flex gap-5 items-center">
-        <div className="flex h-12 w-12 items-center justify-center">
-          <img
-            className="h-10 w-10 object-contain"
-            src="src/assets/images/logo.svg"
-            alt="CLA logo"
-          />
+    <header className="flex justify-between py-5 xl:px-4 px-2 bg-main-100">
+      <div className="flex flex-col xl:flex-row gap-5 items-start xl:items-center">
+        <div className="flex gap-2 items-center">
+          <div className="flex xl:h-12 xl:w-12 items-center justify-center">
+            <img
+              className="h-10 w-10 object-contain"
+              src="src/assets/images/logo.svg"
+              alt="CLA logo"
+            />
+          </div>
+          <p className="text-xl font-semibold">CodeCla</p>
         </div>
-        <p className="text-xl font-semibold">CodeCla</p>
-        <ul className="flex gap-5 text-xl font-semibold">
+        <ul className="flex gap-5 text-lg xl:text-xl font-semibold">
           <li>
             <NavLink to="/challenges">Challenges</NavLink>
           </li>
@@ -23,15 +25,15 @@ export const LeaderHeader: React.FC = () => {
           </li>
         </ul>
       </div>
-      <div className="flex gap-5">
-        <div className="flex h-12 w-12 items-center justify-center">
+      <div className="flex  items-start">
+        <div className="flex h-10 w-10 xl:h-15 xl:w-15 items-center justify-center">
           <img
             src="src/assets/images/user.png"
             alt="user"
-            className="h-10 w-10 object-cover rounded-full"
+            className="object-cover rounded-full"
           />
         </div>
-        <div className="flex items-center justify-center">
+        <div className="flex h-10 w-10 xl:h-15 xl:w-15 items-center justify-center">
           <MdLightMode />
         </div>
       </div>
--- src/pages/leaderboard/page.tsx
+++ src/pages/leaderboard/page.tsx
@@ -5,11 +5,12 @@ import { LeaderHeader } from "../../components/LeaderHeader";
 
 export const LeaderbordPage: React.FC = () => {
   return (
-    <div className="bg-background-100 w-screen h-screen">
+    <div className="bg-background-100 w-screen h-screen gap-2 flex flex-col">
       <LeaderHeader />
-      <div className="flex flex-col gap-2 px-3">
-      <h2 className="font-semibold text-3xl">Leaderboard</h2>
-      <LeaderTable data={dummyData} /></div>
+      <div className="flex flex-col gap-2 p-3">
+        <h2 className="font-semibold xl:text-3xl text-2xl py-2">Leaderboard</h2>
+        <LeaderTable data={dummyData} />
+      </div>
     </div>
   );
 };
