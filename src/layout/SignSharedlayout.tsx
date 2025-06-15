53753bbaaddf1477fdaf8523db9bdbcc1605e73c
matdelp <mathilde.delpeuch@gmail.com>
2025-06-15 10:13:27 +0200

    fix: improve responsiveness of SignSharedlayout by adjusting grid and container classes

--- src/layout/SignSharedlayout.tsx
+++ src/layout/SignSharedlayout.tsx
@@ -10,12 +10,12 @@ export type SignPagesProp = {
 
 export const SignSharedlayout: React.FC<SignPagesProp> = (params) => {
   return (
-    <main className="grid grid-cols-2 w-screen h-screen">
+    <main className="grid xl:grid-cols-2 w-screen h-screen">
       <div className="flex items-center justify-center bg-background-500">
         <img src="src/assets/images/coding.png" alt="coding icon" />
       </div>
       <div className="flex flex-col items-center justify-center bg-background-100 w-full">
-        <div className="flex flex-col items-center justify-center bg-white p-10 rounded-xl w-1/2">
+        <div className="flex flex-col items-center justify-center xl:bg-white p-10 rounded-xl xl:w-1/2 w-full">
           <h2 className="text-main-500 text-xl py-5">{params.title}</h2>
           {params.children}
           <p className="py-1">
