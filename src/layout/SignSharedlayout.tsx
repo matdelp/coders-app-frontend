ea97e1472f2118cc7969a0542e84ca98ee336c33
matdelp <mathilde.delpeuch@gmail.com>
2025-06-15 10:10:11 +0200

    fix: remove fixed height from SignSharedlayout for better responsiveness

--- src/layout/SignSharedlayout.tsx
+++ src/layout/SignSharedlayout.tsx
@@ -15,7 +15,7 @@ export const SignSharedlayout: React.FC<SignPagesProp> = (params) => {
         <img src="src/assets/images/coding.png" alt="coding icon" />
       </div>
       <div className="flex flex-col items-center justify-center bg-background-100 w-full">
-        <div className="flex flex-col items-center justify-center bg-white p-10 rounded-xl w-1/2 h-1/2">
+        <div className="flex flex-col items-center justify-center bg-white p-10 rounded-xl w-1/2">
           <h2 className="text-main-500 text-xl py-5">{params.title}</h2>
           {params.children}
           <p className="py-1">
