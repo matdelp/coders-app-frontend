77a0bd40f6d847ab782aa02a86267e36ac360db0
matdelp <mathilde.delpeuch@gmail.com>
2025-06-16 19:00:21 +0200

    feat: add ProgressBar component for visual representation of challenge progress

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import type { ChallengeData } from "../assets/dummyData/challenges";
+
+type ProgressBarProps = {
+  label: string;
+  style: string;
+  data: ChallengeData;
+};
+
+export const ProgressBar: React.FC<ProgressBarProps> = ({
+  label,
+  style,
+  data,
+}) => {
+  return (
+    <div>
+      <label htmlFor="easy" className="font-light">
+        {label}
+      </label>
+      <div className="w-full h-4 bg-gray-300 rounded-md overflow-hidden">
+        <div className={style} style={{ width: `${data.easy}%` }}>
+          {data.easy}%
+        </div>
+      </div>
+    </div>
+  );
+};
