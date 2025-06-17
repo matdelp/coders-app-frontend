a4629fcc39c8f987b70fcfd532de008fc9fd30ff
matdelp <mathilde.delpeuch@gmail.com>
2025-06-17 16:19:58 +0200

    fix: label

--- src/components/ChallengesTracker.tsx
+++ src/components/ChallengesTracker.tsx
@@ -10,7 +10,7 @@ export const ChallengesTracker: React.FC<ChallengesTrackerProps> = ({
 }) => {
   return (
     <div className="flex flex-col gap-5 py-5">
-      <h2 className="text-center text-xl xl:text-3xl xl:py-5">Completed Challenges:</h2>
+      <h2 className="text-center text-xl xl:text-3xl xl:py-5">Completed Challenges</h2>
       <ProgressBar
         label="Easy"
         style="h-full bg-green-400 rounded-md text-background-100 flex items-center justify-end p-1 text-xs"
