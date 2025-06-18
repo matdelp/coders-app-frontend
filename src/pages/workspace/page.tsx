480323e5889be2a2955ae0aa38ca20d01a8c2445
matdelp <mathilde.delpeuch@gmail.com>
2025-06-18 09:26:36 +0200

    feat: implement split of the screen for workspace page

--- src/pages/workspace/page.tsx
+++ src/pages/workspace/page.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import Split from "react-split";
+import "../../assets/css/split-module.css";
 
 export const WorkspacePage: React.FC = () => {
-  return <div>WorkspacePage</div>;
+  return (
+    <div className="h-screen w-screen">
+      <Split
+        sizes={[50, 50]}
+        minSize={100}
+        expandToMin={false}
+        gutterSize={10}
+        gutterAlign="center"
+        snapOffset={30}
+        dragInterval={1}
+        direction="horizontal"
+        cursor="col-resize"
+        className="flex h-full"
+      >
+        <div className="bg-amber-400 h-full overflow-auto">
+          ChallengeDescription
+        </div>
+        <div className="bg-blue-900 h-full  overflow-auto">Playgroung</div>
+      </Split>
+    </div>
+  );
 };
