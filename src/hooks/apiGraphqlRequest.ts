34d753970ce8acc6769f274103bb3d4ed93d0f93
matdelp <mathilde.delpeuch@gmail.com>
2025-07-31 08:30:40 +0200

    feat: initial set up of graphql query

---
+++ src/hooks/apiGraphqlRequest.ts
@@ -0,0 +1,18 @@
+import { createApi } from "@reduxjs/toolkit/query/react";
+import { graphqlRequestBaseQuery } from "@rtk-query/graphql-request-base-query";
+import { useAuth } from "../store/useAuthStore";
+
+export const gqlApi = createApi({
+  reducerPath: "api",
+  baseQuery: graphqlRequestBaseQuery({
+    url: "http://localhost:3000/graphql/",
+    prepareHeaders: (headers) => {
+      const token = useAuth.getState().token;
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  endpoints: () => ({}),
+});
